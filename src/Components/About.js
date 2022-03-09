73f3bb0161c41a2e068bbf3f1fb7a7ff9972be32
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2022-03-09 23:21:22 +0530

    tweeks

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,11 +18,7 @@ const About = ({ data }) => {
     <section id="about">
       <div className="row">
         <div className="three columns">
-          <img
-            className="profile-pic"
-            src={profilepic}
-            alt="Sonny's Profile Pic"
-          />
+          <img className="profile-pic" src={profilepic} alt="My Profile Pic" />
         </div>
         <div className="nine columns main-col">
           <h2>About Me</h2>
@@ -37,7 +33,9 @@ const About = ({ data }) => {
                 <span>
                   {street}
                   <br />
-                  {city} {state}, {zip}
+                  {state}, {zip}
+                  <br />
+                  {city}
                 </span>
                 <br />
                 <span>{phone}</span>
